7fb496ebd15edab0bbde41b301d5b2a952e628f4
abdalrahman989 <119062548+abdalrahman989@users.noreply.github.com>
2022-11-25 18:05:11 +0300

    Add files via upload

---
+++ picture project/project pictuer _1/project pictuer _1/Form1.cs
@@ -0,0 +1,15 @@
+namespace project_pictuer__1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = "http//www.google.com/intl/en_com/image/srpr/logolw.png";
+        }
+    }
+}
